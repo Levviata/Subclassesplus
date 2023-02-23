004ba6e476d015b07f449f3b0ff2279d82623462
yeahfrick <aaronober11@gmail.com>
2023-02-23 16:55:56 +0100

    Initial commit

---
+++ Content/Items/Weapons/ExampleShortsword.cs
@@ -0,0 +1,43 @@
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Subclassesplus.Content.Items.Weapons
+{
+	public class ExampleShortsword : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Example Shortsword");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() {
+			Item.damage = 8;
+			Item.knockBack = 4f;
+			Item.useStyle = ItemUseStyleID.Rapier; // Makes the player do the proper arm motion
+			Item.useAnimation = 12;
+			Item.useTime = 12;
+			Item.width = 32;
+			Item.height = 32;
+			Item.UseSound = SoundID.Item1;
+			Item.DamageType = DamageClass.MeleeNoSpeed;
+			Item.autoReuse = false;
+			Item.noUseGraphic = true; // The sword is actually a "projectile", so the item should not be visible when used
+			Item.noMelee = true; // The projectile will do the damage and not the item
+
+			Item.rare = ItemRarityID.White;
+			Item.value = Item.sellPrice(0, 0, 0, 10);
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			Recipe recipe = Recipe.Create(ItemID.DirtBlock);
+			recipe.AddIngredient(ItemID.StoneBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+		
+	}
+}
